a06ff92534ac76dbee4de351e08bb0f14cd07677
William <up2195767@myport.ac.uk>
2025-11-28 10:43:23 +0000

    Added basic tests for Sandwich Model

---
+++ test/models/sandwich_test.dart
@@ -0,0 +1,30 @@
+import 'package:sandwich_shop/models/sandwich.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Sandwich Model', () {
+    test('creates a sandwich with correct properties', () {
+      final sandwich = Sandwich(
+        type: SandwichType.veggieDelight,
+        isFootlong: true,
+        breadType: BreadType.wheat,
+      );
+
+      expect(sandwich.name, 'Veggie Delight');
+      expect(sandwich.image, 'assets/images/veggieDelight_footlong.png');
+      expect(sandwich.breadType, BreadType.wheat);
+    });
+
+    test('creates a sandwich with correct properties', () {
+      final sandwich = Sandwich(
+        type: SandwichType.tunaMelt,
+        isFootlong: true,
+        breadType: BreadType.wheat,
+      );
+
+      expect(sandwich.name, 'Tuna Melt');
+      expect(sandwich.image, 'assets/images/tunaMelt_footlong.png');
+      expect(sandwich.breadType, BreadType.wheat);
+    });
+  });
+}
